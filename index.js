a2dcfbb4fcbb68fa0754651fb74caa750b14b526
xhaqur xhariph <shakuritoshariff@gmail.com>
2024-06-27 17:21:18 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+ let result;
+ if(feet > 2500){
+    result = "No can do.";
+  }
+  else if (feet > 2000){
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if(feet >= 400){
+    result = "That will be twenty bucks.";
+  }
+  else {
+  result = "This one is on me!";
+  
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip){
+  case 'generous': return "Thank you so much.";
+  break
+  case 'not as generous': return "Thank you.";
+  break
+  case 'thanks for everything': return "Bye.";
+  break
+  }
+ 
 }
+
+
+
+console.log(scuberGreetingForFeet);
+console.log(ternaryCheckCity);
+console.log(switchOnCharmFromTip);
